2b0bf3fa79bd6e0ac49231195734bbded71d2ec7
vikas gudi <vikasgudii@gmail.com>
2025-11-02 16:21:35 +0530

    admin and backend completed

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -5,11 +5,6 @@ import { addUser, removeUser } from '../controllers/authController.js';
 const authRoutes = express.Router();
 
 
-<<<<<<< HEAD
-//User registration (name, university, class, email, password, payment screenshot)
-authRoutes.post('/register',registerUser);
-=======
->>>>>>> a5442b9 (all backend and ui of admin panel has completed)
 
 //Adding User (name, email)
 authRoutes.post('/add',addUser);
