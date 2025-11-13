eb40b7d82254a59c7a0f8086144ecdddb9048970
ShubhamTiwary914 <shubhamtiwary914@gmail.com>
2025-11-13 12:58:05 +0530

    local setup with mongodb at port=27017 container for backend service

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(
-            'mongodb+srv://vikasgudi:gmail123%40G@uucsc.885eczz.mongodb.net/mydb?retryWrites=true&w=majority'
-        );
-        console.log("MongoDB connected successfully from db.js");
+        const address = process.env.MONGO_URI
+        console.log(`trying to connect mongodb server at: ${address}`) 
+        await mongoose.connect(address);
+        console.log("MongoDB connected successfully from config/db.js");
     } catch (error) {
         console.error("MongoDB connection error:", error);
         process.exit(1);
